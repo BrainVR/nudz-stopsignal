234dfb49561b16c55a6901acd880d42f483e15a1
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-12-05 17:48:51 +0100

    Updates exports and processing with separation of mistakes

--- R/stopsignal-exporter.R
+++ R/stopsignal-exporter.R
@@ -1,4 +1,4 @@
-#' Title
+#' Creates a list of results for a single participant
 #'
 #' @param df_experiment
 #'
@@ -7,23 +7,27 @@
 #'
 #' @examples
 prepare_export <- function(df_experiment, blocks_to_analyze = c(1,2,3)){
-
   blocknumbers <- unlist(lapply(blocks_to_analyze, function(x){rep(x, 4)}))
+
   df_all_combinations <- data.frame(blocknumber = as.character(blocknumbers),
                                     correct_response = rep(c("error", "correct", "error", "correct"),
                                                            length(blocks_to_analyze)),
                                     trialcode = rep(c("stop", "stop", "nostop", "nostop"),
                                                     length(blocks_to_analyze)))
   df_experiment <- filter(df_experiment, blocknumber %in% blocks_to_analyze)
+
+  ## Calculating average response times
   df_out <- df_experiment %>%
     group_by(blocknumber, trialcode, correct_response) %>%
     summarise(mean_rt = mean(values.rt, na.rm = TRUE),
               median_rt = median(values.rt, na.rm = TRUE),
               .groups = "drop") %>%
-    right_join(df_all_combinations, by=colnames(df_all_combinations)) %>%
+    right_join(df_all_combinations, by = colnames(df_all_combinations)) %>%
+    # removing reaction times of trials where no realctions were given - correct stop signal trials
     filter(!(trialcode == "stop" & correct_response == "correct")) %>%
     unite(col = "varname", trialcode, correct_response, blocknumber, sep = "_")
 
+  ## Calculating accuracy of correct responses
   df_out <- df_experiment %>%
     group_by(blocknumber, trialcode) %>%
     summarise(accuracy = sum(correct_response == "correct")/n(),
@@ -32,20 +36,24 @@ prepare_export <- function(df_experiment, blocks_to_analyze = c(1,2,3)){
     select(-c(blocknumber,  trialcode)) %>%
     full_join(df_out, by="varname")
 
+  ## Calculates summative stats for all analyzed blocks together
   df_all <- df_experiment %>%
     group_by(trialcode, correct_response) %>%
     summarise(mean_rt = mean(values.rt, na.rm = TRUE),
               median_rt = median(values.rt, na.rm = TRUE),
               .groups = "drop") %>%
-    mutate(varname=paste(trialcode, correct_response, paste0(blocks_to_analyze, collapse=""), sep="_")) %>%
+    mutate(varname=paste(trialcode, correct_response,
+                         paste0(blocks_to_analyze, collapse=""), sep="_")) %>%
     select(mean_rt, median_rt, varname)  %>%
     pivot_wider(names_from = varname, values_from = c(mean_rt, median_rt))
 
+  ## Accuracies for all analyzed blocks together
   df_all_accuracy <- df_experiment %>%
     group_by(trialcode) %>%
     summarise(accuracy = sum(correct_response == "correct")/n(),
               .groups="drop") %>%
-    mutate(varname=paste("accuracy", trialcode, paste0(blocks_to_analyze, collapse=""), sep="_")) %>%
+    mutate(varname=paste("accuracy", trialcode,
+                         paste0(blocks_to_analyze, collapse=""), sep="_")) %>%
     select(varname, accuracy) %>%
     pivot_wider(names_from = varname, values_from = accuracy)
 
--- R/stopsignal-preprocessor.R
+++ R/stopsignal-preprocessor.R
@@ -17,12 +17,17 @@ preprocess_experiment <- function(df_experiment){
   }
   dat <- df_experiment %>%
     mutate(blocknumber = as.character(expressions.blocknumber),
-           correct_response = ifelse(values.correct == 2, "correct", "error"),
+           correct_signal = ifelse((values.response > 0 & values.signal == 0) |
+                                     (values.response == 0 & values.signal ==1 ), "correct", "error"),
            correct_key = ifelse(values.response == values.stimulus, "correct", "error"),
            correct_key = ifelse(values.response == 0, NA, correct_key),
+           b_correct_response = ((correct_signal  == "correct") & (correct_key == "correct")) |
+             (values.signal == 1 & correct_signal == "correct"),
+           correct_response = ifelse(b_correct_response, "correct", "error"),
            trialcode = recode(trialcode, sstrial = "stop", nstrial="nostop"),
            stopsignalstart = expressions.ssrt) %>%
     select(-starts_with("expressions"))
+  dat[(dat$trialcode == "stop" & dat$correct_response == "correct"), "values.rt"] <- NA_real_
 
   # The any of is there as some datasets do NOT have these columns
   dat <- select(dat, -any_of(c("values.ns_ntotal", "values.ss_ntotal", "values.ssd")))
